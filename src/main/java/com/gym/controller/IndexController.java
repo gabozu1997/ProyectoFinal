9689c46b27e790a0647a4321c3760fbbd614be1b
jassonum26 <jasson2682@gmail.com>
2025-07-07 01:27:20 -0600

    Inicio Actualizado,Implementacion Contacto,FAQ,Info

--- src/main/java/com/gym/controller/IndexController.java
+++ src/main/java/com/gym/controller/IndexController.java
@@ -1,18 +1,39 @@
 package com.gym.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/") //URL al que responde
+    @GetMapping("/") // Inicio
     public String index() {
-        return "index"; // Nombre de la vista en templates a mostrar
+        return "index";
     }
 
-    @GetMapping("/faq")
-    public String faq() {
-        return "faq";
+    @GetMapping("/inscribete")
+    public String inscribete() {
+        return "inscribete";
     }
+
+    @GetMapping("/perfil")
+    public String perfil() {
+        return "perfil";
+    }
+
+    @GetMapping("/nosotros")
+    public String sobreNosotros() {
+        return "SobreNosotros";
+    }
+    @GetMapping("/faq")
+    public String preguntasFrecuentes() {
+        return "PreguntasFrecuentes";
+    }
+    
+    @GetMapping("/contacto")
+public String contacto() {
+    return "Contacto";
 }
+   
+}
+
