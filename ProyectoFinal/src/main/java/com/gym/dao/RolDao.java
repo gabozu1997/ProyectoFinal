083aff22f30a77453c34925cec5745afd52afc41
Gabriel <gabozu97@hotmail.com>
2025-08-18 00:20:17 -0600

    Delete ProyectoFinal directory

--- ProyectoFinal/src/main/java/com/gym/dao/RolDao.java
+++
@@ -1,9 +0,0 @@
-package com.gym.dao;
-
-import com.gym.domain.Rol;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RolDao extends JpaRepository<Rol, Long> {
-    List<Rol> findByIdUsuario(Long idUsuario);
-}
